c374e938adc2bb793b11cb638f83821db2d489d7
Wamuu-hihu <camille.hooreman@student.unamur.be>
2020-03-20 11:11:57 +0100

    J'ai rajouté nos trucs c:

--- module_gr_40 (1).py
+++ module_gr_40 (1).py
@@ -97,81 +97,199 @@ def create_peaks ():
     s[peaks_x3][peaks_y3] = peaks
     s[peaks_x4][peaks_y4] = peaks
     
-def create_cruiser (cruiser_name):
-    """ create cruisers next to the hub
-    parameters
+def create_cruiser (cruiser_name, player_name):
+    """ This fonction creates the cruisers next to the hub.
+    Parameters
     ----------
-    cruiser_name = the name of the cruiser (str)
-    
-    return
-    ------
-    energy = energy of the cruiser (int)
-    structure_points= the structure points of the cruiser (int)
-    distance_attaack = the portée of the attack (int)
-    """
+    cruiser_name : the name of the cruiser (str)
+    player_name : the name of the player (str)
 
-def create_ravitailleur (ravitailleur_name):
-    """ create ravitailleurs next to the hub
-    parameters
+    Return
+    ------
+   all_cruisers : a dico with all the different cruisers (dict)
+   cruiser_name : the name of the cruiser (dict)
+
+    Version
+    -------
+    Specification : Louis Blaimont (17/02)
+    Implémentation : Elodie Fiorentino
+    """
+#là il faut une liste pour l'emplacement du tanker (hub1 ou hub2) mais j'ai pas tout compris
+cruiser_name = {"name" : cruiser_name, "y_coordinate" : y_coordinate, "x_coordonate" : x_coordonnate,
+                "stucture_points" : 100, "energy_capacity" : 400, "moves_cost" : 10, "attack_cost" : 10,"cost_purchase" : 750} 
+all_cruisers = cruiser_name #faut le déclarer dans la fct principale
+return all_cruisers, cruiser_name
+
+def create_tanker (tanker_name, player_name):
+    """ This fonction creates the tankers next to the hub.
+    Parameters
     ----------
-    ravitailleur_name = the name of the ravitailleur (str)
+    tanker_name : the name of the tanker (str)
+    player_name : the name of the player (str)
 
-    return
+    Return
     ------
-    energy = energy of the ravitailleur  (int)
-    structure_points= the structure points of the ravitailleur(int)
-    capicity_energy = the capicity of energy the ravitailleur can take (int)
+    all_tankers : a dico with all the different tankers (dict)
+    tanker_name : the name of the tanker (dict)
+
+    Version
+    -------
+    Specification : Louis Blaimont (17/02)
+    Implémentation : Camille Hooreman 
     """
+#là il faut une liste pour l'emplacement du tanker (hub1 ou hub2) mais j'ai pas tout compris
+tanker_name = {"name" : tanker_name, "y_coordinate" : y_coordinate, "x_coordonate" : x_coordonnate,
+                "stucture_points" : 50, "energy_capacity" : 600, "moves_cost" : 0, "cost_purchase" : 1000} 
+all_tankers = tanker_name #faut le déclarer dans la fonction principale
+return all_tankers, tanker_name
 
 def move (unity_name, column_number, line_number):
-    """ move the unity's where they want
-    parameters
+    """ Move the different unities where it is asked.
+    Parameters
     ----------
     unity_name = the name of the unity that will move (str)
     column_number = the distance in column the unity will do (int)
     line_number = the distance in line the unity will do (int)
-    """
-def give_energy (hub_name, energy_amount):
-    """ give energy to the cruiser
-    parameters
-    ----------
-    hub_name = the name of the cruiser who will recieve the energy (str)
-    energy_amount = the amount of energy gived to the cruiser
 
-    return 
+    Version
+    -------
+    Specification : Louis Blaimont (17/02/20)
+    Implementation : 
+    """
+def give_energy (player_name, tanker_name, cruiser_name, energy_amount):
+    """ Give energy to a cruiser.
+    Parameters
+    ----------
+    player_name = the name of the player
+    tanker_name = the name of the tanker who will give the energy (str)
+    cruiser_name = the name of the cruiser who will recieve the energy (str)
+    energy_amount = the amount of energy gived to the cruiser (int)
+
+    Return 
     ------
-    cruiser_energy = the new energy of the cruiser (int)
-    ravitailleur_energy = the new energy of the ravitalleur (int)   
+    energy_cruiser = the new energy of the cruiser (int)
+    energy_tanker = the new energy of the tanker (int)   
+
+    Version
+    -------
+    Specification : Louis Blaimont (17/02/20), Camille Hooreman (06/03/20)
+    Implementation :  
     """
 def pick_energy (unity_name):
-    """ pick the energy of the hub or peaks 
-    parameters 
+    """  Pick the energy out of the hub or peaks.
+    Parameters 
     ----------
-    unity_name = the name of the hub or peak (str)
+    unity_name = the name of the hub or peak where the tanker take the energy (str)
 
-    return
+    Return
     ------
     hub_energy = the new energy of the hub (int)
     peak_energy = the new energy of the peak (int)
-    ravitailleur_energy = the new energy of the ravitalleur (int)
+    energy_tanker = the new energy of the tanker (int)
+
+    Version
+    -------
+    Specification : Louis Blaimont (17/02/20)
+    Implementation : 
     """
 def attack (unity_name):
-    """ attack an unity of the other player
-    parameters
+    """Attack an unity of the other player.
+    Parameters
     ----------
-    unity_name = the name of the unity attacked (str)
+    unity_name = the name of the attacked unity (str)
+
+    Returns
+    -------
+    energy_unity = the new energy of the unity after being attacked (int)
+
+    Version
+    -------
+    Specification : Louis Blaimont
+    Implementation : 
     """
 def modify_caracter ():
-    """ modify the caracter in the folder"""
-def regeneration ():
-    """regenerate the energy of a hub or a peak"""
+    """ Modify the caracter in the folder
+    
+    Version
+    -------
+    Specification : Louis Blaimont (17/02/20)
+    Implementation : """
+def regeneration (percentage_regen_hubs, percentage_regen_peaks):
+    """Regenerate the energy of a hub or a peak.
+    
+    Parameters
+    ----------
+    percentage_regen_hubs = how much the hubs regenerate after every turn (float)
+    percentage_regen_peaks = how much the peaks regenerate after every turn (float)
+
+    Return
+    ------
+    energy_hub = the new energy of the hubs (int)
+    energy_peaks = the new energy of the peaks (int)
+
+    Version  
+    -------
+    Specification : Camille Hooreman (06/03/20)
+    Implementation : 
+    """
+
+def upgrade_tanker_energy (tanker_name):
+    """ Upgrades the capacity of energy of a tanker
+
+    Parameters 
+    ----------
+    tanker_name : the name of the tanker we want to upgrade (str)
+
+    Return
+    ------
+    tanker_name : the tanker with his new capacity (dict)
+
+    Version
+    -------
+    Specification : Louis Blaimont (17/02/20)
+    Implementation : 
+    """
+if tanker_name["energy_capacity"] = 1200 : 
+    #ne rien faire
+else  : 
+    tanker_name["energy_capacity"] += 100
+    # -600 énergie dans le hub
+
+def upgrade_cruiser_move (cruiser_name):
+    """ Upgrades the moves costs of a cruiser
+
+    Parameters 
+    ----------
+    cruiser_name : the name of the cruiser we want to upgrade (str)
+
+    Return
+    ------
+    cruiser_name : the cruiser with his new move cost (dict)
+
+    Version
+    -------
+    Specification : Louis Blaimont (17/02/20)
+    Implementation : 
+    """
+
+if cruiser_name ["move_cost"] > 5 : 
+    cruiser_name["move_cost"] -= 1
+    #-500 énergie dans le hub
+else : 
+    #ne rien faire
+
+def end_game () : 
+    """ Finish the game and tell who's the winner.
+    
+    Version
+    -------
+    Specification : Camille Hooreman (06/03/20)
+    Implementation : 
+    """
+
+
 def energy_quest ():
     """ launge the game to the end """
     create_map
     if structure_point_hub > 0:
         blabla 
-def upgrade (unity_name, upgrade_sort, amount_upgrade):
-    """ upgrades the unity with different aspects
-    parameters 
-    ----------
-    """
